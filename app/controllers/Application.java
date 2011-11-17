876fbfc70e76011ad69bede26bbc406a8529d097
Jacek Panachida <jacek.panachida@gmail.com>
2011-11-17 22:38:17 +0100

    Added captcha.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
 import play.*;
+import play.cache.*;
 import play.data.validation.*;
+import play.libs.*;
 import play.mvc.*;
 
 import java.util.*;
@@ -26,17 +28,34 @@ public class Application extends Controller {
     
     public static void show(Long id) {
         Post post = Post.findById(id);
-        render(post);
-    }    
+        String randomID = Codec.UUID();
+        render(post, randomID);
+    }
 
-    public static void postComment(Long postId, @Required String author, @Required String content) {
+    public static void postComment(
+            Long postId, 
+            @Required(message="Author is required") String author, 
+            @Required(message="A message is required") String content, 
+            @Required(message="Please type the code") String code, 
+            String randomID) 
+    {
         Post post = Post.findById(postId);
+        validation.equals(
+            code, Cache.get(randomID)
+        ).message("Invalid code. Please type it again");
         if(validation.hasErrors()) {
-            render("Application/show.html", post);
+            render("Application/show.html", post, randomID);
         }
         post.addComment(author, content);
         flash.success("Thanks for posting %s", author);
+        Cache.delete(randomID);
         show(postId);
     }
 
+    public static void captcha(String id) {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
+    }
 }
