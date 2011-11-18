df67d9b86e485779b6a520e36d2a56f9cc91aa9a
Jacek Panachida <jacek.panachida@gmail.com>
2011-11-18 17:43:41 +0100

    Creating a custom editor area.

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -19,7 +19,47 @@ public class Admin extends Controller {
     }
  
     public static void index() {
+        String user = Security.connected();
+        List<Post> posts = Post.find("author.email", user).fetch();
+        render(posts);
+    }
+    
+    public static void form(Long id) {
+        if(id != null) {
+            Post post = Post.findById(id);
+            render(post);
+        }
         render();
+    }
+     
+    public static void save(Long id, String title, String content, String tags) {
+        Post post;
+        if(id == null) {
+            // Create post
+            User author = User.find("byEmail", Security.connected()).first();
+            post = new Post(author, title, content);
+        } else {
+            // Retrieve post
+            post = Post.findById(id);
+            // Edit
+            post.title = title;
+            post.content = content;
+            post.tags.clear();
+        }
+        // Set tags list
+        for(String tag : tags.split("\\s+")) {
+            if(tag.trim().length() > 0) {
+                post.tags.add(Tag.findOrCreateByName(tag));
+            }
+        }
+        // Validate
+        validation.valid(post);
+        if(validation.hasErrors()) {
+            render("@form", post);
+        }
+        // Save
+        post.save();
+        index();
     }
     
 }
