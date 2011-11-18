1fa25b69cbc4e30edb06f663eac4fafefa49682d
Jacek Panachida <jacek.panachida@gmail.com>
2011-11-18 18:46:01 +0100

    Completing the application tests.

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -13,5 +13,11 @@ public class ApplicationTest extends FunctionalTest {
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
     }
-    
+ 
+    @Test
+    public void testAdminSecurity() {
+        Response response = GET("/admin");
+        assertStatus(302, response);
+        assertHeaderEquals("Location", "/login", response);
+    }
 }
