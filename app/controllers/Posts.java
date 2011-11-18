b92e7a5fd45c1531f54af734d47c69d34a44ea61
Jacek Panachida <jacek.panachida@gmail.com>
2011-11-18 17:24:59 +0100

    Adding authentication.

--- app/controllers/Posts.java
+++ app/controllers/Posts.java
@@ -3,5 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
  
+@Check("admin")
+@With(Secure.class)
 public class Posts extends CRUD {    
 }
--- app/controllers/Tags.java
+++ app/controllers/Tags.java
@@ -3,5 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
  
+@Check("admin")
+@With(Secure.class)
 public class Tags extends CRUD {    
 }
